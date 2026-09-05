7ae44d785335f9490dbf67e8cee81e34f3a650e5
felixrydberg <felixryd@gmail.com>
2026-09-05 15:24:16 +0200

    clean:migrations

--- packages/migrations/drizzle/20260905110211_002_container_resources_columns/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "container_version" ADD COLUMN IF NOT EXISTS "cpu" text;--> statement-breakpoint
-ALTER TABLE "container_version" ADD COLUMN IF NOT EXISTS "memory" text;--> statement-breakpoint
